9403787c4132b9b3d2489f3a227b3dbc7613c613
valeyellow <sumit.mishtra.4693@gmail.com>
2021-10-29 23:50:39 +0530

    Added route handler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+import express from "express";
+import cors from "cors";
+import config from "config";
+import logger from "./utils/logger";
+import connect from "./utils/connect";
+import routes from "./routes";
+
+const port = config.get<number>("port");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, async () => {
+  logger.info(`Server is running on port:${port}`);
+
+  routes(app);
+
+  await connect();
+});
