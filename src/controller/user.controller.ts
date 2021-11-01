3a276255d3010bd4f50574485cda8c62fabdbbd7
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-01 22:53:03 +0530

    Validating the response and creating a new user record upon successful validation

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
+import { CreateUserInput } from "../schema/user.schema";
 import { createUser } from "../service/user.service";
 
-export async function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
   try {
     const user = await createUser(req.body);
-    return user;
+    return res.send(user);
   } catch (e: any) {
     res.status(409).send(e.message); // 409 --> Conflict (user already exists)
   }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,13 +1,20 @@
 import { DocumentDefinition } from "mongoose";
-import { userInfo } from "os";
 import UserModel, { UserDocument } from "../models/user.model";
 import logger from "../utils/logger";
+import { omit } from "lodash";
 
-export async function createUser(input: DocumentDefinition<UserDocument>) {
+export async function createUser(
+  input: DocumentDefinition<
+    Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">
+  >
+) {
   try {
     const user = await UserModel.create(input);
-    return user;
+
+    // omit password field before sending the user details
+    return omit(user.toJSON(), "password");
   } catch (e) {
     logger.error(e);
+    throw new Error(e);
   }
 }
