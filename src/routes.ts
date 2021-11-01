72e938d810bd780e44091cf3a477bb2c8d026dd7
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-01 22:51:18 +0530

    Added endpoint to create a user

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import verifyResources from "./middleware/verifyResources";
+import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
   app.get("/healthCheck", (req: Request, res: Response) => {
     res.send("Connection is successful!");
   });
+
+  app.post("/api/users", verifyResources(createUserSchema), createUserHandler);
 }
 
 export default routes;
