059c004b41cd12b67c93036049ddcb7abf9d353a
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-03 23:37:04 +0530

    Added endpoints to create a new session (login) and get the current session

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,12 @@
 import { Express, Request, Response } from "express";
+import {
+  createSessionHandler,
+  getSessionsHandler,
+} from "./controller/session.controller";
 import { createUserHandler } from "./controller/user.controller";
+import requireUser from "./middleware/requireUser";
 import verifyResources from "./middleware/verifyResources";
+import createSessionSchema from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
@@ -9,6 +15,14 @@ function routes(app: Express) {
   });
 
   app.post("/api/users", verifyResources(createUserSchema), createUserHandler);
+
+  app.post(
+    "/api/sessions",
+    verifyResources(createSessionSchema),
+    createSessionHandler
+  );
+
+  app.get("/api/sessions", requireUser, getSessionsHandler);
 }
 
 export default routes;
