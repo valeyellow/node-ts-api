d09c81010d28b319e65964b18bd2d45198504db1
valeyellow <sumit.mishtra.4693@gmail.com>
2021-11-03 23:34:34 +0530

    Added a middleware to verify that res.locals contains the userId of the current user

---
+++ src/middleware/requireUser.ts
@@ -0,0 +1,15 @@
+// verify whether the res.locals has userId added
+
+import { Request, Response, NextFunction } from "express";
+
+const requireUser = async (req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals.user;
+
+  if (!user) {
+    return res.sendStatus(403);
+  }
+
+  next();
+};
+
+export default requireUser;
